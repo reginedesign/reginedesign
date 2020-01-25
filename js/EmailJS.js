46eeedd2ed866e73ba5e3982869184e9b499a55a
Muhd Zaki <zazakikiki@gmail.com>
2020-01-25 08:22:05 +0800

    V2

--- js/EmailJS.js
+++ js/EmailJS.js
@@ -1,26 +1,40 @@
 (function(){
-  emailjs.init("user_cQLFD7zU7dBix5qiT6hoz");
+	emailjs.init("user_cQLFD7zU7dBix5qiT6hoz");
 
-  var myform = $("form#contactForm");
-  myform.submit(function(event){
-  	event.preventDefault();
+	var myform = $("form#contactForm");
+	myform.submit(function(event){
+		event.preventDefault();
+
 
     // Change to your service ID, or keep using the default service
-    var service_id = "regine";
+    var service_id = "gmail";
     var template_id = "template_WSo9Hfs2";
 
     myform.find("button").text("Sending...");
     
     
-	  emailjs.sendForm(service_id,template_id,"contactForm")
-		  .then(function(){
-			alert("Sent!");
-			 myform.find("button").text("Send");
-			 window.location.reload();
-		  }, function(err) {
-			 alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-			 myform.find("button").text("Send");
-		  });
-		return false;
-  })
+    emailjs.sendForm(service_id,template_id,"contactForm")
+    .then(function(){
+    	new Noty({
+    		theme: 'nest',
+    		type: 'success',
+    		layout: 'topRight',
+    		text: 'Your message have been sent! :)'
+    	}).show();
+    	myform.find("button").text("Send");
+    	myform.find("input[type=text], textarea").val("");
+    }, function(err) {
+    	new Noty({
+    		theme: 'nest',
+    		killer: true,
+    		timeout: 1000,
+    		type: 'error',
+    		layout: 'topRight',
+    		text: 'Oh no! Something went wrong! :('
+    	}).show();
+    	console.log(err)
+    	myform.find("button").text("Send");
+    });
+    return false;
+})
 })();
