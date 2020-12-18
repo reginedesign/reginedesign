2256a2e1a727658cd7ef98e14239230924ac6b2f
Zaki <Zaki.jst@gmail.com>
2020-12-18 16:49:30 +0800

    websote add filtered portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var fp = null;
 $(document).ready(function () {
   // typing animation
   (function ($) {
@@ -75,12 +76,14 @@ $(document).ready(function () {
 
   // initiate full page scroll
 
-  $("#fullpage").fullpage({
-    scrollBar: true,
+$("#fullpage").fullpage({
+    scrollBar: false,
     responsiveWidth: 400,
     navigation: true,
     navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
     anchors: ["home", "about", "portfolio", "contact", "connect"],
+    scrollOverflow: true,
+    scrollOverflowReset: true,
     menu: "#myMenu",
     fitToSection: true,
 
@@ -163,56 +166,8 @@ $(document).ready(function () {
       }
     });
   });
-
-  /*//ajax form
-  $(function () {
-    // Get the form.
-    var form = $("#ajax-contact");
-
-    // Get the messages div.
-    var formMessages = $("#form-messages");
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-      // Stop the browser from submitting the form.
-      e.preventDefault();
-
-      // Serialize the form data.
-      var formData = $(form).serialize();
-
-      // Submit the form using AJAX.
-      $.ajax({
-        type: "POST",
-        url: $(form).attr("action"),
-        data: formData
-      })
-        .done(function (response) {
-          // Make sure that the formMessages div has the 'success' class.
-          $(formMessages).removeClass("error");
-          $(formMessages).addClass("success");
-
-          // Set the message text.
-          $(formMessages).text(response);
-
-          // Clear the form.
-          $("#name").val("");
-          $("#email").val("");
-          $("#message").val("");
-        })
-        .fail(function (data) {
-          // Make sure that the formMessages div has the 'error' class.
-          $(formMessages).removeClass("success");
-          $(formMessages).addClass("error");
-
-          // Set the message text.
-          if (data.responseText !== "") {
-            $(formMessages).text(data.responseText);
-          } else {
-            $(formMessages).text(
-              "Oops! An error occured and your message could not be sent."
-            );
-          }
-        });
-    });
-  });*/
 });
+
+function enableBtn(){
+  document.getElementById("submitFormBtn").disabled = false;
+}
