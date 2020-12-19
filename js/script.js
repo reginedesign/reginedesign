e56d36965be084e831567c97b721086712013430
Zaki <Zaki.jst@gmail.com>
2020-12-20 02:18:10 +0800

    normalScrollElements

--- js/script.js
+++ js/script.js
@@ -84,6 +84,7 @@ $("#fullpage").fullpage({
     anchors: ["home", "about", "portfolio", "contact", "connect"],
     scrollOverflow: true,
     menu: "#myMenu",
+    normalScrollElements:".modal",
     fitToSection: true,
 
     afterLoad: function (anchorLink, index) {
